9eb05ffc747b4e668f10a9b8be6c848bc2f2a3cd
Alex <mysteryman208@gmail.com>
2014-03-02 11:04:18 -0500

    Im on! alex

--- minecraft/com/krebs/mod/Anythinghonestly.java
+++ minecraft/com/krebs/mod/Anythinghonestly.java
@@ -49,6 +49,8 @@ public class Anythinghonestly {
                     return new ItemStack(PoopItem, 1, 0);
             }
     };
+    
+    //hurray This is alex!
 
     static Item PoopItem;
     static Item CleanPoopItem;
